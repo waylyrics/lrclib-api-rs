afe0f0a9b845c12e7edee086ca090dbc201f9eb0
poly000 <1348292515a@gmail.com>
2024-11-09 10:28:02 +0800

    enhance: avoid `None, None` case by api

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ use http::{Method, Request};
 mod types;
 pub use types::*;
 
+type Result<T> = std::result::Result<T, ApiError>;
+
 const BASE_URL: &str = "https://lrclib.net";
 const DEFAULT_USER_AGENT: &str = concat!("LRCAPIWrapper/", env!("CARGO_PKG_VERSION"));
 
@@ -57,7 +59,7 @@ impl LRCLibAPI {
         artist_name: &str,
         album_name: Option<&str>,
         duration: Option<u64>,
-    ) -> Result<Request<()>, ApiError> {
+    ) -> Result<Request<()>> {
         let mut query_params: Vec<(&str, Cow<'_, str>)> = vec![
             ("track_name", track_name.into()),
             ("artist_name", artist_name.into()),
@@ -87,7 +89,7 @@ impl LRCLibAPI {
             .map_err(ApiError::from)
     }
 
-    pub fn get_lyrics_by_id(&self, id: u64) -> Result<Request<()>, ApiError> {
+    pub fn get_lyrics_by_id(&self, id: u64) -> Result<Request<()>> {
         let uri = format!("{}/api/get/{}", &self.base_url, id);
 
         Request::builder()
@@ -98,21 +100,28 @@ impl LRCLibAPI {
             .map_err(ApiError::from)
     }
 
-    pub fn search_lyrics(
+    pub fn search_lyrics_query(&self, query: &str) -> Result<Request<()>> {
+        let query_string = format!("q={query}");
+
+        let uri = format!("{}/api/search?{}", &self.base_url, query_string);
+
+        Request::builder()
+            .method(Method::GET)
+            .uri(uri)
+            .header("User-Agent", &self.user_agent)
+            .body(())
+            .map_err(ApiError::from)
+    }
+
+    pub fn search_lyrics_detailed(
         &self,
-        query: Option<&str>,
-        track_name: Option<&str>,
+        track_name: &str,
         artist_name: Option<&str>,
         album_name: Option<&str>,
-    ) -> Result<Request<()>, ApiError> {
+    ) -> Result<Request<()>> {
         let mut query_params = vec![];
+        query_params.push(("track_name", track_name));
 
-        if let Some(q) = query {
-            query_params.push(("q", q));
-        }
-        if let Some(track) = track_name {
-            query_params.push(("track_name", track));
-        }
         if let Some(artist) = artist_name {
             query_params.push(("artist_name", artist));
         }
@@ -136,7 +145,27 @@ impl LRCLibAPI {
             .map_err(ApiError::from)
     }
 
-    pub fn request_publish_challenge(&self) -> Result<Request<()>, ApiError> {
+    #[deprecated(
+        since = "0.2.0",
+        note = "Instead of a manual check, we split search methods"
+    )]
+    pub fn search_lyrics(
+        &self,
+        query: Option<&str>,
+        track_name: Option<&str>,
+        artist_name: Option<&str>,
+        album_name: Option<&str>,
+    ) -> Result<Request<()>> {
+        match (query, track_name) {
+            (None, None) => Err(ApiError::MissingFieldExists),
+            (_, Some(track_name)) => {
+                self.search_lyrics_detailed(track_name, artist_name, album_name)
+            }
+            (Some(query), None) => self.search_lyrics_query(query),
+        }
+    }
+
+    pub fn request_publish_challenge(&self) -> Result<Request<()>> {
         let uri = format!("{}/api/request-challenge", &self.base_url);
 
         Request::builder()
@@ -151,7 +180,7 @@ impl LRCLibAPI {
         &self,
         lyrics: &LyricsData,
         publish_token: &str,
-    ) -> Result<Request<String>, ApiError> {
+    ) -> Result<Request<String>> {
         let uri = format!("{}/api/publish", &self.base_url);
         if lyrics.album_name.is_none() || lyrics.duration.is_none() {
             Err(ApiError::MissingFieldExists)?
